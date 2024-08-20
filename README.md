e5a788d2a71609331e2ef3cb5bfe2fdceed91b12
Taha <TahaMunawar43@outlook.com>
2024-08-20 21:16:03 +0500

    Bot starts on command, and filename can be given. Only mentor can run

--- README.md
+++ README.md
@@ -2,16 +2,22 @@
 
 Bots that upload code from file when it changes to discord server
 
-put an index.html inside the same folder with bot.py and it will send the file in the server whenever it changes
+put some files in the same folder as bot.py then use relative path to watch them and it will send the file in the server whenever it changes
 
 make sure to provide in env
 
-1. `CHANNEL` which is channel ID where you want to upload the code
-2. `TAG_USER` user ID of user who will be pinged to the uploaded code, whenever it changes
+1. `MENTOR_ID` ID of moderator or teacher role that will be able to start the bot watching code
+2. `STUDENT_ID` ID of student or whichever role that will get pinged to the code
 3. `BOT_TOKEN` which is your bot token 
 
-## Usage
+## Setup
 
 You will have to create the bot yourself and copy paste in the code and run it
 
 make sure to give the bot the neccesary permissions
+
+## Usage
+
+type in `$watch filepath` in any channel and it would send the code whenever it's changed in that channel
+
+you can use "\" to escape spaces in filepath
--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+from typing import Union
 import discord
 import asyncio
 import os
@@ -8,36 +9,114 @@ with open(".env", "r") as file:
         [key, value] = line.split("=")
         env_vars[key] = value
 
+STUDENT_ID = int(env_vars["STUDENT_ID"])
+MENTOR_ID = int(env_vars["MENTOR_ID"])
+BOT_TOKEN = env_vars["BOT_TOKEN"]
+
+MessageableChannel = Union[
+    discord.TextChannel,
+    discord.StageChannel,
+    discord.VoiceChannel,
+    discord.Thread,
+    discord.DMChannel,
+    discord.GroupChannel,
+    discord.PartialMessageable,
+]
+
+
+def escapeable_split(string: str, splitter: str = " ", include_splitter: bool = False):
+    split_list = []
+    word = ""
+    ESCAPE = "\\"
+    escaped = False
+    for letter in string:
+        if letter == ESCAPE:
+            escaped = True
+            continue
+        if letter == splitter and not escaped:
+            split_list.append(word)
+            word = ""
+            if not include_splitter:
+                continue
+        elif escaped:
+            escaped = False
+            if letter != splitter:
+                word += ESCAPE
+        word += letter
+    if word != "":
+        split_list.append(word)
+    return split_list
+
+
+def has_file_updated(file, current_stamp):
+    new_stamp = os.stat(file).st_mtime
+    return new_stamp != current_stamp
+
+
 class FileUpdateBot(discord.Client):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
-        self.filename = "./index.html"
-        self.stamp = os.stat(self.filename).st_mtime
-
-    async def setup_hook(self) -> None:
-        self.bg_task = self.loop.create_task(self.file_update_check())
 
     async def on_ready(self):
         print("Bot is ready...")
 
-    async def file_update_check(self):
+    async def on_message(self, message):
+        if message.content.startswith("$"):
+            await handle_commands(message, self)
+
+    async def watch_file(self, filepath: str, channel: MessageableChannel):
         await self.wait_until_ready()
-        channel = await self.fetch_channel(int(env_vars["CHANNEL"]))
+        stamp = os.stat(filepath).st_mtime
 
         while not self.is_closed():
-            new_stamp = os.stat(self.filename).st_mtime
-            if new_stamp != self.stamp:
-                self.stamp = new_stamp
-                text = ""
-                with open(self.filename, "r") as file:
-                    text = file.read()
-                text = f"## Code Changed \n```html\n{text}\n```\n<@{env_vars["TAG_USER"]}>"
+            if has_file_updated(filepath, stamp):
+                stamp = os.stat(filepath).st_mtime
+                code = ""
+                with open(filepath, "r") as file:
+                    code = file.read()
+                code = f"## Code Changed \n```html\n{code}\n```\n<@&{STUDENT_ID}>"
 
-                await channel.send(text)
+                await channel.send(code)
 
             await asyncio.sleep(3)
 
-intents = discord.Intents.default()
-intents.guilds = True
-client = FileUpdateBot(intents=intents)
-client.run(env_vars["BOT_TOKEN"])
+
+async def send_denial(message: discord.Message):
+    await message.channel.send("Only mentors can watch for file changes")
+
+
+async def find_is_mentor(message: discord.Message):
+    isMentor = False
+    author = message.author
+    if not isinstance(author, discord.Member):
+        return False
+    for role in author.roles:
+        if role.id == MENTOR_ID:
+            isMentor = True
+    return isMentor
+
+
+async def handle_commands(message: discord.Message, client: FileUpdateBot):
+    isMentor = find_is_mentor(message)
+
+    if not isMentor:
+        await send_denial(message)
+        return
+
+    content = message.content
+    split = escapeable_split(content)
+    match split[0]:
+        case "$watch":
+            filepath = split[1]
+            client.bg_task = client.loop.create_task(
+                client.watch_file(filepath, message.channel)
+            )
+            await message.channel.send(f"Watching {filepath} 🤖")
+
+
+if __name__ == "__main__":
+    intents = discord.Intents.default()
+    intents.guilds = True
+    intents.message_content = True
+    client = FileUpdateBot(intents=intents)
+    client.run(env_vars["BOT_TOKEN"])
